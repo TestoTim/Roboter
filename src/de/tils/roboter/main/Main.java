90da425f670a7dd7c9a3b846e87a750b28e33b8e
Madakai <madakai.code@gmail.com>
2019-11-13 10:56:54 +0100

    add infos

--- src/de/tils/roboter/main/Main.java
+++ src/de/tils/roboter/main/Main.java
@@ -2,8 +2,28 @@ package de.tils.roboter.main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	/**
+	 * Infos form project
+	 */
+	public static class Info {
+		static final String VERSION = "1.0";
+		static final String[] CODER = { "Tim", "Nils" };
 	}
 
+	public static void main(String[] args) {
+		projectInfos();
+	}
+
+	/**
+	 *	print project infos in console
+	 */
+	private static void projectInfos() {
+		System.out.println(String.format(
+				" ______         __           __              \n" +
+				"|   __ \\.-----.|  |--.-----.|  |_.-----.----.\n" +
+				"|      <|  _  ||  _  |  _  ||   _|  -__|   _|\n" +
+				"|___|__||_____||_____|_____||____|_____|__|  \n" +
+				"   Version: %s  |  Coder: %s and %s             ",
+				Info.VERSION, Info.CODER[0], Info.CODER[1]));
+	}
 }
